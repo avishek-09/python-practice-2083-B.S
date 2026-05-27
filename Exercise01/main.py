be80d0faca7733149f46fc4d681ed09a11ff1d7e
Avishek Ranjan <raz.avishek45@gmail.com>
2026-05-27 17:19:51 +0545

    Addede first python practice exercise

---
+++ Exercise01/main.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+years = 2026 + (100 - age)
+
+num = int(input("Enter any number: "))
+
+for x in range(0 , num):
+    print(f"Hey {name} you will turn 100 years old in {years} A.D.\n")
+
+print("Program Ends Here!!!")
