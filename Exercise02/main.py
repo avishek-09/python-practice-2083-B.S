c79441510d0083092afced426ea77d283c547986
Avishek Ranjan <raz.avishek45@gmail.com>
2026-05-29 11:27:14 +0545

    Added my Exercise-02 program

--- Exercise02/main.py
+++ Exercise02/main.py
@@ -1,16 +1,24 @@
 # A Shopping Cart program to understand the use of list and user inputs 
 
-Foods = []
-price = []
+foods = []
+prices = []
 total = 0
 
 while True:
-    Food = input("Enter the food you want to buy(press q to quit): ")
-    if Food.lower() == "q":
+    food = input("Enter the food you want to buy(press q to quit): ")
+    if food.lower() == "q":
         break
     else:
-        Foods.append(Food)
+        price = float(input("Enter the price : Nrs."))
+        prices.append(price)
+        foods.append(food)
 
 print("---- Your Cart------")
-for Food in Foods:
-    print(Food)
+for food in foods:
+    print(food)
+
+for price in prices:
+    total += price
+
+print()
+print(f"Your amount total is Nrs.{total}")
