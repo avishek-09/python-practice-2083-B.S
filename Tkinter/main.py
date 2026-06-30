0cb3a8ba16d97ff517a66d9efdedb3f1f70af5d1
Avishek Ranjan <raz.avishek45@gmail.com>
2026-06-30 14:53:54 +0545

    Made Kilometer to Meter Conveter using tkinter moduel

--- Tkinter/main.py
+++ Tkinter/main.py
@@ -14,4 +14,28 @@ label3.pack()
 
 root.mainloop()
 
+## ----Project for KM to Meter Converter------
+
+root = tk.Tk()
+root.title("KM to M Converter")
+root.geometry("600x400")
+
+def convert():
+    km = float(input.get())
+    meter = km * 1000
+    result.config(text = f"In Meter: {meter}")
+
+label1 = tk.Label(text = "Enter distance in KM: ", font = ("Arial", 13))
+label1.place(x = 130 , y = 30)
+
+input = tk.Entry()
+input.place(x = 300, y = 33)
+
+button = tk.Button(text = "Convert", command = convert)
+button.place(x = 430 , y = 29)
+
+result = tk.Label(text = "In Meter: ", font = ("Arial", 13))
+result.place(x= 180 , y = 65)
+
+root.mainloop()
 
