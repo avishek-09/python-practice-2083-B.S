67d47f66494748580a3cae11e56e3a92535ffeb4
Avishek Ranjan <raz.avishek45@gmail.com>
2026-07-09 23:59:21 +0545

    Addded updates to snake game

--- snakeGame/main.py
+++ snakeGame/main.py
@@ -6,6 +6,10 @@ screen = pygame.display.set_mode((800, 500))
 x = 20 
 y = 20 
 speed = 5
+velocity_x = 0
+velocity_y = 0
+width = 50
+height = 25
 
 playing = True
 while playing:
@@ -17,23 +21,29 @@ while playing:
             if event.key == pygame.K_ESCAPE:
                 playing = False
             elif event.key == pygame.K_RIGHT:
-                x = x + speed
+                velocity_x = speed
+                velocity_y = 0
             elif event.key == pygame.K_LEFT:
-                x = x - speed
+                velocity_x = -speed
+                velocity_y = 0
             elif event.key == pygame.K_UP:
-                y = y - speed
+                velocity_y = - speed
+                velocity_x = 0
             elif event.key == pygame.K_DOWN :
-                y = y + speed
+                velocity_y = speed
+                velocity_x = 0
 
 
     # x = x + 0.1
+    x += velocity_x
+    y += velocity_y
 
     screen.fill((255,255,255))
 
-    pygame.draw.rect(screen, (255,0,0), [x,y,100,100])
+    pygame.draw.rect(screen, (0,255,100), [x,y,width,height])
 
     pygame.display.update()
 
-    pygame.time.Clock().tick(1080)
+    pygame.time.Clock().tick(80)
 
 pygame.quit()
